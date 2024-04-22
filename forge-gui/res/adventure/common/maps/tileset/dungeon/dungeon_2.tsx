d4ac1939daa16e77032978831a42c53d2139db01
Simisays <67333662+Simisays@users.noreply.github.com>
2024-04-22 19:53:02 +0200

    update

---
+++ forge-gui/res/adventure/common/maps/tileset/dungeon/dungeon_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Rogue Adventure Tileset" tilewidth="16" tileheight="16" tilecount="16384" columns="128">
+ <image source="ra.png" width="363" height="856"/>
+</tileset>
